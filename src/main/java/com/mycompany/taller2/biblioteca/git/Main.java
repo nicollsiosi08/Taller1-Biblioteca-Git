19971fb4539ee3fe0645cca2c7c38b73e6a19590
njohanagomez <njohanagomez@unicesar.edu.co>
2026-08-18 10:52:56 -0500

    feat: inicializar proyecto Maven de biblioteca

---
+++ src/main/java/com/mycompany/taller2/biblioteca/git/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller2.biblioteca.git;
+
+/**
+ *
+ * @author Nicoll Gomez Siosi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
